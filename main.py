49028116058f3c24a63248303b25fb21a63cca50
Stefan-Vo <38333563+Stefan-Vo@users.noreply.github.com>
2024-05-04 03:36:58 +0000

    Main Classes Finished + UCS finished

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from node import Node
 from tree import Tree
 
 def main():
-    initial_state = [1, 2, 0, 4, 5, 3, 7, 8, 6]
+    initial_state = [0, 1, 2, 4, 5, 3, 7, 8, 6]
     goal_state = [1,2,3,4,5,6,7,8,0]
 
     operators = ["UP", "DOWN", "LEFT", "RIGHT"]
